aebe8124a7e6f42a5df47f7c9b3cf8442683e9d9
GUILHERME RABAIOLI VIEIRA <guilherme.rabaioli@sicoobamazonia.com.br>
2024-02-26 12:25:20 -0400

    Finalização do Projeto

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const limit = document.getElementById("ate");
 const init = document.getElementById("de");
 const quant = document.getElementById("quantidade");
+const text = document.getElementById("resultado");
+const btnRein = document.getElementById("btn-reiniciar")
 
+let randomNumbers = [];
+let numero;
 
 function sortear(){
-    let number = (Math.random() * limit.value) + 1;
-    if(number > limit && number < init){
-        sortear()
-    }else{
-        console.log(parseInt(number))
-        return parseInt(number) 
+    for (let i = 0; i  < quant.value; i++) {
+        numero = obterNumeroAleatorio();
     }
-    console.log(number)
+    text.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${randomNumbers}</label>`
+    btnRein.className = "container__botao"
 }
 
+function obterNumeroAleatorio () {
+    let number = (Math.random() * limit.value) + 1;
+    if(number > limit.value && number < init.value){
+        sortear()
+    }else{
+        randomNumbers.push(parseInt(number));
+        return parseInt(number) 
+}
+}
+
+function reiniciar () {
+    quant.value ="";
+    init.value = ""; 
+    limit.value = "";
+    randomNumbers = [];
+    btnRein.className = "container__botao-desabilitado"
+    text.innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`
+}
